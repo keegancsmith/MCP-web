bc5deb7099b5f6311265c7dffdcec65d73a85efa
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2012-08-03 11:38:09 +0200

    Add some readonly properties of TronGame to admin view.

--- mcpweb/admin.py
+++ mcpweb/admin.py
@@ -15,8 +15,14 @@ class TronGameAdmin(admin.ModelAdmin):
                        ('date_created', 'last_played'),
                        'game_state')
         }),
+        ('Readonly Properties', {
+            'classes': ('collapse',),
+            'fields': (('players', 'winners'),
+                       ('current_player', 'current_user')),
+        }),
     )
-    readonly_fields = ('player1_token', 'player2_token')
+    readonly_fields = ('player1_token', 'player2_token',
+                       'current_player', 'current_user', 'winners', 'players')
     list_filter = ('winner',)
     list_display = ('id', 'player1', 'player2', 'winner',
                     'date_created', 'last_played')
